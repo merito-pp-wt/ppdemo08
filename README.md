7d3248ed003c089ca8283bd92a9c3195053bccac
Wojtek <wojciech.thomas@pwr.edu.pl>
2024-01-28 00:49:33 +0100

    Finished assignment

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Przykład 08 - Tablice i kolekcje
 
-Strona przykładowa
+W tym ćwiczeniu przedstawiono sposób korzystania z tablic.
+
+Tablice w języku Java są obiektami, tak więc mają właściwości takie jak np. `length`.
+Ponieważ rozmiar tablicy nie może się zmienić, `length` nie jest metodą, ale polem
+o stałej wartości. 
+
+Znajdź w dokumentacji Javy opis klasy java.util.Arrays, która zawiera różne funkcje
+umożliwiające manipulowanie tablicami (wypełnianie wartością, porównanie, sortowanie i in.)
+
 
 ## Zadanie
+
+Dodaj do klasy `App` następujące metody:
+
+1. Metodę `min()` znajdującą najmniejszą wartość w tablicy.
+2. Metodę `average1()` obliczającą średnią wartość liczb zawartych w tablicy. Wykorzystaj 
+   metodę `sumOfElements()`.
+3. Metodę `average2()` obliczającą średnią wartość liczb zawartych w tablicy, ale bez pomocy
+   metody `sumOfElements()`.
+4. Metodę `last()` zwracającą wartość ostatniego elementu w tablicy (elementu o największym indeksie).
--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
+/**
+ * Klasa App demonstrująca użycie tablic.
+ */
 public class App {
 	public void tableOperations() {
 		int[] table1, table2;
@@ -15,9 +18,57 @@ public class App {
 			table1[i] = table2[table2.length - i - 1];
 		}
 
+		if( Arrays.equals(table1, table2)) {
+			System.out.println("W obu tablicach jest to samo");
+		} else {
+			System.out.println("Tablice mają różną zawartość");
+		}
+
 		for(int i : table1){                       // for-each i in table1
 			System.out.println("Element " + i);
 		}
+
+		System.out.println("Największy element w tablicy: " + findMaxElement(table1));
+		System.out.println("Suma elementów tablicy: " + sumOfElements(table1));
 	}
 
+	/**
+	 * Zwraca największy element w tablicy.
+	 * Jeśli tablica jest pusta lub nie istnieje, to zwracana jest wartość 0.
+	 *
+	 * @param t przeszukiwana tablica
+	 * @return największa wartość w tablicy lub 0
+	 */
+	public int findMaxElement(int[] t) {
+		if( t == null || t.length == 0)
+			return 0;
+
+		int max = t[0];
+		for(int element : t) {
+			if( max < element ) {
+				max = element;
+			}
+		}
+
+		return max;
+	}
+
+	/**
+	 * Zwraca sumę elementów tablicy liczb całkowitych
+	 * @param table tablica sumowanych liczb
+	 * @return suma elementów lub 0 jeśli tablica nie istnieje
+	 */
+	public int sumOfElements(int[] table) {
+		if(table == null) {
+			return 0;
+		}
+
+		int sum = 0;
+
+		for(int element : table) {
+			sum += element;
+		}
+
+		return sum;
+	}
 }
